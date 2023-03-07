442accb5901577948d012bdf4af6b22c9e1cf2e5
Pierre SOMNY <113438729+pierresomny@users.noreply.github.com>
2023-03-07 22:46:03 +0100

    chore(school-app) commenting controller + add structures for School

--- src/main/java/com/sfeiropensource/schoolapp/controller/SchoolController.java
+++ src/main/java/com/sfeiropensource/schoolapp/controller/SchoolController.java
@@ -3,31 +3,84 @@ package com.sfeiropensource.schoolapp.controller;
 import com.sfeiropensource.schoolapp.model.School;
 import com.sfeiropensource.schoolapp.repository.SchoolRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("schools")
 public class SchoolController {
 
     @Autowired
-    private SchoolRepository repo;
+    private SchoolRepository schoolRepository;
 
+    /**
+     * Retrieve all schools from database
+     *
+     * @return List<School>
+     */
+    @GetMapping("/")
+    public ResponseEntity<List<School>> getAll() {
+        return ResponseEntity.ok(schoolRepository.findAll());
+    }
+
+    /**
+     * Add a school to the database
+     *
+     * @param school School
+     * @return School - School is returned to provide new generated id
+     */
     @PostMapping("/add")
-    public String add(@RequestBody School school){
-        repo.save(school);
-        return "Added Successfully";
+    public ResponseEntity<School> add(@RequestBody School school) {
+        School savedSchool = schoolRepository.save(school);
+        return ResponseEntity.ok(savedSchool);
     }
 
-    @GetMapping("/findAll")
-    public List<School> getBooks() {
-        return repo.findAll();
+    /**
+     * Retrieve a specific school attached to an id
+     *
+     * @param id int - Unique identifier of a school
+     * @return School | String
+     */
+    @GetMapping("/get/{id}")
+    public ResponseEntity<?> get(@PathVariable int id) {
+        Optional<School> school = schoolRepository.findById(id);
+        if (school.isPresent()) {
+            return ResponseEntity.ok(school);
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No School is attached to this id");
     }
 
+    /**
+     * Update a school
+     *
+     * @param id            int - Unique identifier of a school
+     * @param updatedSchool School - updated
+     * @return String
+     */
+    @PutMapping(value = "/update/{id}")
+    public ResponseEntity<String> update(@PathVariable("id") int id, @RequestBody School updatedSchool) {
+        Optional<School> school = schoolRepository.findById(id);
+        if (school.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No School is attached to this id");
+        }
+        // TODO: Search why example update each property at a time and doesn't just save the object.
+        schoolRepository.save(updatedSchool);
+        return ResponseEntity.ok("School updated");
+    }
+
+    /**
+     * Delete a school
+     *
+     * @param id int - Unique identifier of a school
+     * @return String
+     */
     @DeleteMapping("/delete/{id}")
-    public String deleteBook(@PathVariable int id){
-        repo.deleteById(id);
-        return "Deleted Successfully";
+    public ResponseEntity<String> delete(@PathVariable int id) {
+        schoolRepository.deleteById(id);
+        return ResponseEntity.ok("School deleted");
     }
 }
--- src/main/java/com/sfeiropensource/schoolapp/model/School.java
+++ src/main/java/com/sfeiropensource/schoolapp/model/School.java
@@ -1,18 +1,30 @@
 package com.sfeiropensource.schoolapp.model;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-// Annotations
+import java.util.List;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Document(collection = "School")
 public class School {    // Attributes
-    @Id private int id;
+    @Id
+    private int id;
     private String title;
-    private String public_summary;
-    private String github_link;
+    // Optional ?
+    private String image;
+    private String publicSummary;
+    private String duration;
+    private List<String> objectives;
+    private List<String> prerequisites;
+    private String document;
+    private String githubLink;
+    private List<User> teachers;
+    private User professor;
+    private String status;
 }
